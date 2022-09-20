fc2746650ad174fac0cd65413ffdc07ab0e8b7d9
Johannes Danielsson <danielssonjohannes@gmail.com>
2022-09-20 11:54:14 +0200

    Main
    Making text so I can test out the file writer

---
+++ src/folderMain/Main.java
@@ -0,0 +1,10 @@
+package folderMain;
+
+import folderFile.FileService;
+
+public class Main {
+    public static void main(String[] args) {
+        FileService ne= new FileService();
+        ne.createdFile();
+    }
+}
