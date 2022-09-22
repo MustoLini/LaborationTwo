daa7866bfd3802deafaaa3602b808a6ec74598fc
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2022-09-22 11:30:30 +0200

    Update to main.java
    testting out the new feture

--- src/folderMain/Main.java
+++ src/folderMain/Main.java
@@ -2,9 +2,13 @@ package folderMain;
 
 import folderFile.FileService;
 
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         FileService ne= new FileService();
         ne.createdFile();
+        ne.productPlacement();
+
     }
 }
