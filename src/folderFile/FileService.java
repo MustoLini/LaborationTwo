b38000a937d0486e27cae0fb82a71d3177f6ebf2
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2022-09-22 13:07:55 +0200

    FileSerice.java
    Early feature of adding product still not work because it overrides the old txt text

--- src/folderFile/FileService.java
+++ src/folderFile/FileService.java
@@ -9,14 +9,13 @@ import java.util.Scanner;
 
 public class FileService {
 
-
     private Scanner in = new Scanner(System.in);
     private String nameOfFile;
 
     private ArrayList<Product>productsInStore= new ArrayList<>();
     String s;
     private File file = new File("storeStorageFile.txt");
-    public FileWriter createdFile() {
+    public FileWriter createdFile() throws FileNotFoundException {
 
 
         FileWriter output = null;
@@ -43,11 +42,10 @@ public class FileService {
                     System.out.println("What is the category of the product? ");
                     String category = in.nextLine();
                     output.write(category + ",");
+                    output.close();
 
-
+                    this.productPlacement();
                 }
-
-                output.close();
 
             } catch (FileNotFoundException e) {
                 throw new RuntimeException(e);
@@ -61,18 +59,57 @@ public class FileService {
     }
     public ArrayList<Product> productPlacement() throws FileNotFoundException {
         Scanner scanner = new Scanner(file);
+        Product newProduct=null;
 
         while (scanner.hasNext() || scanner.hasNextLine()){
             String writer= scanner.next();
             String[] token= writer.split(",");
             for (int i = 0; i < token.length; i+=4) {
                 System.out.println(token[i]);
-                Product newProduct= new Product(token[i],Integer.parseInt(token[i+1]),Integer.parseInt(token[i+2]), token[i+3]);
+                newProduct= new Product(token[i],Integer.parseInt(token[i+1]),Integer.parseInt(token[i+2]), token[i+3]);
                 productsInStore.add(newProduct);
+
             }
+            System.out.println(productsInStore.get(0));
 
         }
         return productsInStore;
+
+    }
+    public FileWriter addProduct(){
+
+        FileWriter output = null;
+        if (file.exists() && !file.isDirectory()) {
+            try {
+                output = new FileWriter(file);
+                System.out.println("How many products do you want to add to the Text File? ");
+                int amountOfItemsAdded = in.nextInt();
+                in.nextLine();
+
+                for (int i = 0; i < amountOfItemsAdded; i++) {
+                    System.out.println("What is the name of the product? ");
+                    String name = in.nextLine();
+                    output.write(name + ",");
+                    System.out.println("What is the price of the product? ");
+                    int price = in.nextInt();
+                    in.nextLine();
+                    output.write(price + ",");
+                    System.out.println("What is the count of the product? ");
+                    int count = in.nextInt();
+                    in.nextLine();
+                    output.write(count + ",");
+                    System.out.println("What is the category of the product? ");
+                    String category = in.nextLine();
+                    output.write(category + ",");
+                    output.close();
+                    this.productPlacement();
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return output;
 
     }
 
