87178fbb207e3a11c13f860d8edcd466670a0e8e
Johannes Danielsson <danielssonjohannes@gmail.com>
2022-10-03 16:56:06 +0200

    Main.java
    Testing out features. Making new menu class to do other funktions.

--- src/folderMain/Main.java
+++ src/folderMain/Main.java
@@ -1,15 +1,43 @@
 package folderMain;
 
 import folderFile.FileManager;
+import folderProducts.Product;
 import menuFile.Menu;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         FileManager re= new FileManager();
         re.fileReader();
-        System.out.println(re.getProductData());
+
+        Menu menu= new Menu();
+        menu.checkWhatIsInsideFile();
+        menu.printProductInStock();
+        menu.printProductThatIsLowestPrice();
+        //System.out.println(re.getProductData());
+        ArrayList<Product> productList= re.getProductData();
+       // System.out.println(productList);
+        int min=productList.get(0).getPrice();
+        //Get the lowest price of the products.
+        productList.sort(Comparator.comparingInt(Product::getPrice));
+        productList.forEach(System.out::println);
+       // System.out.println(productList.stream().min(Comparator.comparing(Product::getPrice)));
+        //Get the lowest price of alla product and print them out;
+        for (int i = 1; i < productList.size(); i++) {
+            if (productList.get(i).getPrice() < min) {
+                min = productList.get(i).getPrice();
+            }
+        }
+        for (Product po:productList) {
+       //     System.out.println(po);
+        }
+     //   System.out.println();
+
+
 
     }
 }
