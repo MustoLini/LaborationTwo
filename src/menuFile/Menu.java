b71da8dcbd0618306d1155b590a9976bd6c2c5ef
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2022-10-05 14:51:05 +0200

    Menu.java
    Adding so you can get list in cheapest to the expencivest and adding remove product from stock method

--- src/menuFile/Menu.java
+++ src/menuFile/Menu.java
@@ -3,12 +3,18 @@ package menuFile;
 import folderFile.FileManager;
 import folderProducts.Product;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Scanner;
 
 public class Menu {
     FileManager productStockFile= new FileManager();
     ArrayList<Product> productArrayList= productStockFile.getProductData();
+    Scanner in= new Scanner(System.in);
     public void checkWhatIsInsideFile(){
         productStockFile.fileReader();
     }
@@ -20,5 +26,26 @@ public class Menu {
     public void printProductThatIsLowestPrice(){
         productArrayList.sort(Comparator.comparingInt(Product::getPrice));
         printProductInStock();
+    }
+    public void printProductsThatIsHighestPrice(){
+        productArrayList.sort(Comparator.comparingInt(Product::getPrice).reversed());
+        printProductInStock();
+    }
+    public void RemoveProductFromStock(){
+        int productToRemove= in.nextInt();
+        in.nextLine();
+        if (productToRemove<=0){
+            System.out.println("You can't remove nothing");
+        }
+        if (productToRemove>=1){
+            productArrayList.remove(productToRemove-1);
+            try {
+                BufferedWriter writer=  new BufferedWriter(new FileWriter("storeStorageFile.txt"));
+                productStockFile.AddIntoFile(writer);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 }
