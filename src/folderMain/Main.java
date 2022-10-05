b46b43c251e22a0b20d3c32a6f7da2168b739baf
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2022-10-05 14:51:30 +0200

    Main
    testing out my methods

--- src/folderMain/Main.java
+++ src/folderMain/Main.java
@@ -16,25 +16,28 @@ public class Main {
 
         Menu menu= new Menu();
         menu.checkWhatIsInsideFile();
-        menu.printProductInStock();
+
         menu.printProductThatIsLowestPrice();
+        menu.printProductsThatIsHighestPrice();
+        menu.printProductInStock();
+        menu.RemoveProductFromStock();
         //System.out.println(re.getProductData());
-        ArrayList<Product> productList= re.getProductData();
+       // ArrayList<Product> productList= re.getProductData();
        // System.out.println(productList);
-        int min=productList.get(0).getPrice();
+        //int min=productList.get(0).getPrice();
         //Get the lowest price of the products.
-        productList.sort(Comparator.comparingInt(Product::getPrice));
-        productList.forEach(System.out::println);
+        //productList.sort(Comparator.comparingInt(Product::getPrice));
+        //productList.forEach(System.out::println);
        // System.out.println(productList.stream().min(Comparator.comparing(Product::getPrice)));
         //Get the lowest price of alla product and print them out;
-        for (int i = 1; i < productList.size(); i++) {
-            if (productList.get(i).getPrice() < min) {
-                min = productList.get(i).getPrice();
-            }
-        }
-        for (Product po:productList) {
+        //for (int i = 1; i < productList.size(); i++) {
+            //if (productList.get(i).getPrice() < min) {
+                //min = productList.get(i).getPrice();
+            //}
+        //}
+        //for (Product po:productList) {
        //     System.out.println(po);
-        }
+        //}
      //   System.out.println();
 
 
