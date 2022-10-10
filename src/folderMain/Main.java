653b19c48d6c04dd76983370bd9d263bfeda3e62
Johannes Danielsson <danielssonjohannes@gmail.com>
2022-10-10 14:28:28 +0200

    Changing up Main method with product klass?

--- src/folderMain/Main.java
+++ src/folderMain/Main.java
@@ -12,9 +12,8 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
 
-        firstChoiceToStartOrExitProgram();
 
-
+        choiceToStartOrExitProgram();
 
         //menu.printProductThatIsLowestPrice();
         //menu.printProductsThatIsHighestPrice();
@@ -40,23 +39,24 @@ public class Main {
 
     }
 
-    private static void firstChoiceToStartOrExitProgram() {
+    private static void choiceToStartOrExitProgram() {
         System.out.println("""
                 1:Start Program:
                 2:Exit Program:
                 Please Type in 1 or 2 to make your choice: 
-                
+                                
                 """);
         int choice = in.nextInt();
         in.nextLine();
 
         switch (choice) {
 
-            case 1 -> choicesYouCanGet();
+            case 1 -> printChoices();
             case 2 -> menu.exitProgram();
         }
     }
-    private static void choicesYouCanGet(){
+
+    private static void printChoices() {
         menu.startProgram();
         System.out.println("""
                 1: Would you like to get all the products in the file:
@@ -64,21 +64,25 @@ public class Main {
                 3: Would you like to remove a product from the file:
                 4: Would you like to get the highest price of all products:
                 5: Would you like to get the smallest price of all products:
+                E: Exit Program:
                 Please Type in a number to make your choice: 
-                
+                                
                 """);
-        int choice = in.nextInt();
-        in.nextLine();
-        switch (choice){
-            case 1-> menu.printProductInStock();
-            case 2->menu.addProductsToList();
-            case 3-> menu.removeProductFromStock();
-            case 4-> menu.printProductsThatIsHighestPrice();
-            case 5-> menu.printProductThatIsLowestPrice();
+        choicesToPickOf();
 
+
+    }
+
+    private static void choicesToPickOf() {
+        String choice = in.nextLine();
+
+        switch (choice) {
+            case "1" -> menu.printProductInStock();
+            case "2" -> menu.addProductsToList();
+            case "3" -> menu.removeProductFromStock();
+            case "4" -> menu.printProductsThatIsHighestPrice();
+            case "5" -> menu.printProductThatIsLowestPrice();
+            case "E", "e" -> menu.exitProgram();
         }
-
-
-
     }
 }
