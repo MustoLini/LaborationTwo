cb2d99f0924d642f7c1af6caf7f0b19003839393
MustoLini <danielssonjohannes@gmail.com>
2022-10-13 15:22:55 +0200

    Update Main.java

--- src/folderMain/Main.java
+++ src/folderMain/Main.java
@@ -10,33 +10,7 @@ public class Main {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) throws FileNotFoundException {
-
-
-
         choiceToStartOrExitProgram();
-
-        //menu.printProductThatIsLowestPrice();
-        //menu.printProductsThatIsHighestPrice();
-        //menu.printProductInStock();
-        //System.out.println(re.getProductData());
-        // ArrayList<Product> productList= re.getProductData();
-        // System.out.println(productList);
-        //int min=productList.get(0).getPrice();
-        //Get the lowest price of the products.
-        //productList.sort(Comparator.comparingInt(Product::getPrice));
-        //productList.forEach(System.out::println);
-        // System.out.println(productList.stream().min(Comparator.comparing(Product::getPrice)));
-        //Get the lowest price of alla product and print them out;
-        //for (int i = 1; i < productList.size(); i++) {
-        //if (productList.get(i).getPrice() < min) {
-        //min = productList.get(i).getPrice();
-        //}
-        //}
-        //for (Product po:productList) {
-        //     System.out.println(po);
-        //}
-        //   System.out.println();
-
     }
 
     private static void choiceToStartOrExitProgram() {
@@ -46,6 +20,7 @@ public class Main {
                 Please Type in 1 or 2 to make your choice: 
                                 
                 """);
+
         int choice = in.nextInt();
         in.nextLine();
 
@@ -54,22 +29,25 @@ public class Main {
             case 1 -> printChoices();
             case 2 -> menu.exitProgram();
         }
+
     }
 
     private static void printChoices() {
         menu.startProgram();
-        System.out.println("""
-                1: Would you like to get all the products in the file:
-                2: Would you like to add a new product to the file:
-                3: Would you like to remove a product from the file:
-                4: Would you like to get the highest price of all products:
-                5: Would you like to get the smallest price of all products:
-                E: Exit Program:
-                Please Type in a number to make your choice: 
-                                
-                """);
-        choicesToPickOf();
 
+        while (true) {
+            System.out.println("""
+                    1: Would you like to get all the products in the file:
+                    2: Would you like to add a new product to the file:
+                    3: Would you like to remove a product from the file:
+                    4: Would you like to get the highest price of all products:
+                    5: Would you like to get the smallest price of all products:
+                    E: Exit Program:
+                    Please Type in a number to make your choice: 
+                                    
+                    """);
+            choicesToPickOf();
+        }
 
     }
 
