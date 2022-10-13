f8654a26e3dad46e4c2f065f02bd25ab4fe5f923
MustoLini <danielssonjohannes@gmail.com>
2022-10-13 15:22:58 +0200

    Update Product.java

--- src/folderProducts/Product.java
+++ src/folderProducts/Product.java
@@ -2,6 +2,7 @@ package folderProducts;
 
 public class Product {
 
+    private int id;
     private String name;
     private String category;
     private int price;
@@ -10,7 +11,11 @@ public class Product {
     public Product(){
         //Generic
     }
-    public Product(String name, int price, int count, String category){
+
+
+
+    public Product(int id, String name, int price, int count, String category){
+        setId(id);
         setName(name);
         setPrice(price);
         setCount(count);
@@ -20,6 +25,13 @@ public class Product {
 
     public void setCount(int count) {
         this.count = count;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public int getCount() {
@@ -54,8 +66,8 @@ public class Product {
 
     @Override
     public String toString() {
-        return "Product:" +
-                "Name:" + name +
+        return  "ID:"+id +
+                ", Name:" + name +
                 ", Category:" + category  +
                 ", Price:" + price +
                 ", Count:" + count ;
